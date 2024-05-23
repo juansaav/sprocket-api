aaa3fee7834b1f46cf52b39dc4818debf0888709
Juan Saavedra <juansaav_@hotmail.com>
2024-05-23 11:17:03 -0300

    Edit start script, add migrate name to initial migration

--- start.sh
+++ start.sh
@@ -2,12 +2,12 @@
 # Start Docker Compose in detached mode
 docker-compose up -d
 # Wait for the database to be ready
-until docker exec powerflex-test-db-1 pg_isready -U user; do
+until docker exec sprocket-api-db-1 pg_isready -U user; do
   >&2 echo "Postgres is unavailable - sleeping"
   sleep 2
 done
 >&2 echo "Postgres is up - executing command"
 # Build and start the
-npm run prisma-migrate
+npm run prisma-migrate --name init
 npm run build
 npm run dev
